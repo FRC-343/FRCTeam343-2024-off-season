78df1a770e06b192cf4c00a268ad6d1dd2785a3c
bmxcmx <74631712+bmxcmx@users.noreply.github.com>
2024-10-28 09:09:12 -0400

    Add files via upload

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRCTeam343-2024-off-season";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "74b7f119cc5f3b506d7e2b292d31c02c023911aa";
+  public static final String GIT_DATE = "2024-10-24 16:24:42 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-10-24 23:03:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1729825417449L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
