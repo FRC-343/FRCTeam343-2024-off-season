45f7834c28211714846d3559caef46fb0c59fe48
bmxcmx <cadenwhitfield86@gmail.com>
2024-11-21 19:03:27 -0600

    Correct module init

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRCTeam343-2024-off-season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "f1dceb4c8764b189b4bfd97774f845f5b0171c54";
-  public static final String GIT_DATE = "2024-11-14 19:46:49 EST";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "909277333fe605ac9b05ba957ed17dc45eb05293";
+  public static final String GIT_DATE = "2024-11-20 19:15:00 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-11-20 19:13:16 EST";
-  public static final long BUILD_UNIX_TIME = 1732147996150L;
+  public static final String BUILD_DATE = "2024-11-21 20:00:30 EST";
+  public static final long BUILD_UNIX_TIME = 1732237230079L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
@@ -67,17 +67,17 @@ public class ModuleIOTalonFX implements ModuleIO {
         driveTalon = new TalonFX(12, "busman"); // back left
         turnTalon = new TalonFX(1);
         cancoder = new CANcoder(6);
-        absoluteEncoderOffset = new Rotation2d(-1); // MUST BE CALIBRATED
+        absoluteEncoderOffset = new Rotation2d(-Math.PI); // MUST BE CALIBRATED
         break;
       case 2:
         driveTalon = new TalonFX(13, "busman"); // Front right
-        turnTalon = new TalonFX(4);
+        turnTalon = new TalonFX(2);
         cancoder = new CANcoder(7);
         absoluteEncoderOffset = new Rotation2d(Math.PI); // MUST BE CALIBRATED
         break;
       case 3:
         driveTalon = new TalonFX(14, "busman"); // front left
-        turnTalon = new TalonFX(2);
+        turnTalon = new TalonFX(4);
         cancoder = new CANcoder(5);
         absoluteEncoderOffset = new Rotation2d(-Math.PI / 2); // MUST BE CALIBRATED
         break;
